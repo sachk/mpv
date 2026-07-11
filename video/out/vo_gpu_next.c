e3132ec6ab8f0dffcd48781544ced37b84c584aa
Sacha Korban <sk@sachk.com>
2026-07-11 10:39:14 +1000

    fix(webos): exclude unavailable libmpv OpenGL backend

--- video/out/vo_gpu_next.c
+++ video/out/vo_gpu_next.c
@@ -3032,6 +3032,7 @@ AV_NOWARN_DEPRECATED(
     p->flush_cache = p->paused || !p->next_opts->inter_preserve;
 }
 
+#if HAVE_GL && defined(PL_HAVE_OPENGL)
 struct native_resource_entry {
     const char *name;
     size_t size;
@@ -3345,6 +3346,7 @@ const struct render_backend_fns render_backend_gpu_next = {
     .perfdata = libmpv_perfdata,
     .destroy = libmpv_destroy,
 };
+#endif
 
 const struct vo_driver video_out_gpu_next = {
     .description = "Video output based on libplacebo",
